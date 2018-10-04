c2d4baddc6f5f9e7e6360bd5e94a65a9865947e9
WaysideTester <dylanmurray2119@gmail.com>
2018-10-04 18:20:28 -0500

    initialized document structure

---
+++ javascripts/components/petComponent.js
@@ -0,0 +1,5 @@
+const testFunction = () => {
+    console.log("petComponent linked");
+};
+
+export default {testFunction};
---
+++ javascripts/main.js
@@ -0,0 +1,16 @@
+import events from "./events.js";
+import petData from "./data/petdData.js";
+import petComponent from "./components/petComponent.js";
+
+const testFunction = () => {
+    console.log("events linked");
+};
+
+const init = () => {
+    testFunction();
+    events.testFunction();
+    petData.testFunction();
+    petComponent.testFunction();
+};
+
+init();

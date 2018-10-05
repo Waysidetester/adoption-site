3488733500d94a9a410cf88946a49388457fac0f
WaysideTester <dylanmurray2119@gmail.com>
2018-10-04 21:14:30 -0500

    completed unfiltering functionality

--- javascripts/events.js
+++ javascripts/events.js
@@ -4,10 +4,12 @@ const filterAnimal = (animalId) => {
     const etarget = document.getElementById(animalId);
     etarget.addEventListener("click", (e) => {
         const clicked = e.currentTarget.innerHTML;
-        
         console.log(clicked)
         let cardsSorted = [];
         const cards = document.getElementsByClassName("col-4");
+        if (cards.length < 30){
+            petComponent.makeCards(petComponent.petObject);
+        } else {
         for (let i = 0; i < cards.length; i++){
         let animalType = cards[i].childNodes[1].childNodes[7].innerHTML;
         animalType = animalType.toUpperCase();
@@ -16,6 +18,7 @@ const filterAnimal = (animalId) => {
             }
         } 
         petComponent.cardLooper(cardsSorted);
+        }
     })
 };
 

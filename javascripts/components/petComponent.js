d57b9bb12265ae554a4cf07c273000936fe4258d
WaysideTester <dylanmurray2119@gmail.com>
2018-10-04 20:07:36 -0500

    perfect loop close #7

--- javascripts/components/petComponent.js
+++ javascripts/components/petComponent.js
@@ -6,4 +6,26 @@ const printToDom = (divId, stringToPrint) => {
     document.getElementById(divId).innerHTML = stringToPrint;
 };
 
-export default {testFunction, printToDom};
+let petObject = [];
+
+const makeCards = (cardArray) => {
+    let newstring = ``
+    for (let i = 0; i < cardArray.length; i++){
+        newstring += `<div class="col-4">
+                        <div class="card border-info mb-3" style="max-width: 18rem;">
+                            <div class="card-header text-center">${cardArray[i].name}</div>
+                            <img class="card-img-top" src="${cardArray[i].imageUrl}" alt="${cardArray[i].name}">
+                            <div class="card-body text-info">
+                                <p class="card-text text-center">${cardArray[i].color}</p>    
+                                <p class="card-text text-center">${cardArray[i].specialSkill}</p>
+                            </div>
+                            <div class="card-footer text-muted text-center">
+                                ${cardArray[i].type}
+                            </div>
+                        </div>
+                    </div>`;
+    }
+    printToDom("printHere", newstring);
+};
+
+export default {testFunction, printToDom, makeCards, petObject};
--- javascripts/data/petdData.js
+++ javascripts/data/petdData.js
@@ -1,10 +1,14 @@
+import petComponent from "../components/petComponent.js"
+
 const testFunction = () => {
     console.log("petdData linked");
 };
 
 function executeOnLoad () {
     let data = JSON.parse(this.responseText);
-    console.log(data.pets);
+    petComponent.petObject = data.pets;
+    console.log(petComponent.petObject);
+    petComponent.makeCards(petComponent.petObject);
 };
 
 function executeOnError () {
